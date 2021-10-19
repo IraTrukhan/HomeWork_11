43836706a9ee5659822b24758e02aeb2ea9d73d3
IraTrukhan <iryna.viaz@gmail.com>
2021-10-19 18:59:48 +0300

    add  type check

--- task2.js
+++ task2.js
@@ -1,12 +1,12 @@
 let num = prompt("Please enter number", "5");
-if (checkEven(num) == true)
-alert (num + " - it's even\n" + num + " * 8 = " + num*8);
+// console.log(num + "    " + typeof(+num));
+if (num * 0 == 0){
+(checkEven(num) == true) ? alert (num + " - it's even\n" + num + " * 8 = " + num * 8):
+alert (num + " - it's otherwise\n" + num + " * 9 = " + num * 9);
+}
 else
-    alert (num + " - it's otherwise\n" + num + " * 9 = " + num*9);
+alert("not number");
 
 function checkEven(a){
-if (a % 2 == 0)
-return true;
-else
-return false;   
+return (a % 2 == 0)? true : false;
 }
