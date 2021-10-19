e5e87a40643a457d37214e6160b710b11e0a301b
IraTrukhan <iryna.viaz@gmail.com>
2021-10-19 18:40:30 +0300

    add function to task2

--- task2.js
+++ task2.js
@@ -1,8 +1,12 @@
 let num = prompt("Please enter number", "5");
-
-if (num % 2 == 0){
-    alert (num + " - it's even\n" + num + " * 8 = " + num*8);
-}
-else{
+if (checkEven(num) == true)
+alert (num + " - it's even\n" + num + " * 8 = " + num*8);
+else
     alert (num + " - it's otherwise\n" + num + " * 9 = " + num*9);
+
+function checkEven(a){
+if (a % 2 == 0)
+return true;
+else
+return false;   
 }
